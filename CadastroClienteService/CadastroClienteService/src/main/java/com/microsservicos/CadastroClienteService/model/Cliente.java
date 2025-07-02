a4ec72d042bdd494d39ce149d06f803d6df6f796
Rafael <rafinhagvianna@gmail.com>
2025-07-02 12:41:58 -0300

    CadastroClienteService

---
+++ CadastroClienteService/CadastroClienteService/src/main/java/com/microsservicos/CadastroClienteService/model/Cliente.java
@@ -0,0 +1,93 @@
+package com.microsservicos.CadastroClienteService.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String senha;
+    private String agencia;
+    private String conta;
+
+    public Cliente() { }
+
+    public Cliente(String nome, String cpf, String telefone,
+                   String email, String senha, String agencia, String conta) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.senha = senha;
+        this.agencia = agencia;
+        this.conta = conta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getConta() {
+        return conta;
+    }
+
+    public void setConta(String conta) {
+        this.conta = conta;
+    }
+}
