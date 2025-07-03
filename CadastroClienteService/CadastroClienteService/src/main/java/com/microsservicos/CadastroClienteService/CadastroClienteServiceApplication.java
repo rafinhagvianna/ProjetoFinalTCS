98dba1d4c5e731f43b9a387145c2c67bd98f9512
Rafael de Godoy Vianna <91953884+rafinhagvianna@users.noreply.github.com>
2025-07-03 15:48:58 -0300

    Add files via upload

---
+++ CadastroClienteService/CadastroClienteService/src/main/java/com/microsservicos/CadastroClienteService/CadastroClienteServiceApplication.java
@@ -0,0 +1,11 @@
+package com.microsservicos.CadastroClienteService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroClienteServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroClienteServiceApplication.class, args);
+	}
+}
