eefb9f76e475bf081c2c3cede2d689eaa4c640e0
Luiz Fernando Moreira Domênico <luizfmd21@gmail.com>
2025-07-04 12:16:27 -0300

    Add files via upload

---
+++ CadastroClienteService/src/main/java/com/microsservicos/CadastroClienteService/repository/ClienteRepository.java
@@ -0,0 +1,13 @@
+package com.microsservicos.CadastroClienteService.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.microsservicos.CadastroClienteService.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByEmail(String email);
+}
