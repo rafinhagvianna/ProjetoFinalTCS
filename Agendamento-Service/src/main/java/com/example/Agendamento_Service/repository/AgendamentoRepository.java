d9ce45147a83da992dbf68146508c645a4edce8d
gabrielbachega1 <bachegagabriel@gmail.com>
2025-07-10 22:01:54 -0300

    .

--- Agendamento-Service/src/main/java/com/example/Agendamento_Service/repository/AgendamentoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Agendamento_Service.repository;
-
-import com.example.Agendamento_Service.model.Agendamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface AgendamentoRepository extends JpaRepository<Agendamento, UUID> {
-}
